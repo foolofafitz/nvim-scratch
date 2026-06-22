32d6686b044cba928c2c91440d8b9ce568a85bca
foolofafitz <slarty79@pm.me>
2026-06-23 03:02:55 +1000

    add a couple of toggles

---
+++ after/plugin/toggles.lua
@@ -0,0 +1,21 @@
+-- Ensure your leader key is set (usually in init.lua)
+vim.g.mapleader = " "
+
+-- 1. Mini.Pairs Toggle
+local function toggle_pairs()
+    vim.g.minipairs_disable = not vim.g.minipairs_disable
+    print("Mini.pairs: " .. (vim.g.minipairs_disable and "OFF" or "ON"))
+end
+vim.keymap.set("n", "<leader>up", toggle_pairs, { desc = "Toggle Auto Pairs" })
+
+-- 2. Native Diagnostics Toggle
+local function toggle_diagnostics()
+    if vim.diagnostic.is_enabled() then
+        vim.diagnostic.enable(false)
+        print("Diagnostics: OFF")
+    else
+        vim.diagnostic.enable(true)
+        print("Diagnostics: ON")
+    end
+end
+vim.keymap.set("n", "<leader>ud", toggle_diagnostics, { desc = "Toggle Diagnostics" })
